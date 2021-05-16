53b115691149fd1eb8bd6378806a23fe7204057c
mstanka <mice78.mm@gmail.com>
2021-05-16 10:25:49 +0200

    remove starter text

--- src/index.jsx
+++ src/index.jsx
@@ -5,12 +5,9 @@ import './style.css';
 const App = () => (
   <div className="container">
     <header>
-      <div className="logo"></div>
-      <h1>Webová aplikace</h1>
+      <h1>O Projektu</h1>
     </header>
-    <main>
-      <p>Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí <a href="https://www.npmjs.com/package/create-czechitas-app">create-czechitas-app</a>.</p>
-    </main>
+    <main>Main section</main>
     <footer>
       <p>Czechitas, Digitální akademie: Web</p>
     </footer>
