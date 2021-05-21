e91e2b66e5f35fa8f358c79d317d0b4f4a6aa71d
Kateřina Levová <katerina.levova@gmail.com>
2021-05-21 16:58:29 +0200

    Small updates in 'About us'

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,32 +1,36 @@
 import React from 'react';
-// import PhotoKata from 'src/img/fotka Kata.jpg';
+import PhotoKata from '../img/fotkakata.jpg';
 
 const About = () => {
   return (
     <>
-      <div className="page-content">
+      <section className="page-content">
         <h1> O nás </h1>
         <h2> Markéta </h2>
-        <img src=""></img>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi ab
-          culpa quaerat quasi nostrum sed illum iste, facilis magni, natus
-          laboriosam sapiente corporis vel. Possimus iure quam laborum maxime
-          illo.
-        </p>
+        <section className="about-us marketa">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi ab
+            culpa quaerat quasi nostrum sed illum iste, facilis magni, natus
+            laboriosam sapiente corporis vel. Possimus iure quam laborum maxime
+            illo.
+          </p>
+          <img src="" alt="" className="img-us"></img>
+        </section>
         <h2> Káťa </h2>
-        {/* <img src alt="Fotka Káťa" />; */}
-        <p>
-          Na ostrově jsem strávila 6 měsíců jako au-pair. Žila jsem v napůl
-          české a napůl reunionské rodině, spřátelila jsem se místními studenty
-          a celou jsem od všech sondovala tipy na výlety, které Vám ráda předám.
-          Jsem spíše plážový a koupací typ, tipy na hory dostanete zase od
-          Markét. :) Ostrov má spoustu specifik, na které je třeba dávat si
-          pozor, jako např. nevyzpytatelnost počasí a dopravy, ale také
-          nejkrásnější západy slunce, panoramata hor a modře čisté laguny a
-          moře. Budu se těšit, že z našich zážitků něco využijete!
-        </p>
-      </div>
+        <section className="about-us kata">
+          <img src={PhotoKata} alt="Fotka Káťa" className="img-us" />;
+          <p>
+            Na ostrově jsem strávila 6 měsíců jako au-pair. Žila jsem v napůl
+            české a napůl reunionské rodině, spřátelila jsem se místními
+            studenty a celou jsem od všech sondovala tipy na výlety, které Vám
+            ráda předám. Jsem spíše plážový a koupací typ, tipy na hory
+            dostanete zase od Markét. :) Ostrov má spoustu specifik, na které je
+            třeba dávat si pozor, jako např. nevyzpytatelnost počasí a dopravy,
+            ale také nejkrásnější západy slunce, panoramata hor a modře čisté
+            laguny a moře. Budu se těšit, že z našich zážitků něco využijete!
+          </p>
+        </section>
+      </section>
     </>
   );
 };
