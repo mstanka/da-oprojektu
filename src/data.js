564c5fa20df0f0c26ad87e1e04744e6416a788cc
mstanka <mice78.mm@gmail.com>
2021-05-22 22:05:11 +0200

    add photo gallery to update Project page

---
+++ src/data.js
@@ -0,0 +1,102 @@
+export const photos = [
+  {
+    src: './assets/img-1.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: './assets/img-2.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: './assets/img-3.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: './assets/img-4.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: './assets/img-5.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: './assets/img-6.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: './assets/img-7.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: './assets/img-8.jpg',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: './assets/img-9.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: './assets/img-10.jpg',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: './assets/img-11.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: './assets/img-12.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: './assets/img-13.jpg',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: './assets/img-14.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: './assets/img-15.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: './assets/img-16.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: './assets/img-17.jpg',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: './assets/img-18.jpg',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: './assets/img-19.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: './assets/img-20.jpg',
+    width: 4,
+    height: 3,
+  },
+];
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import Gallery from 'react-photo-gallery';
+import { photos } from '../data.js';
 
 const Project = () => {
-  return <div>Project page</div>;
+  return (
+    <div className="page-content">
+      <h1>O projektu</h1>
+      <p>
+        Etiam dignissim sem non eleifend tempor. Nullam eget suscipit mauris.
+        Nunc vel magna vitae mauris pulvinar efficitur. Integer a lorem non urna
+        hendrerit consequat. Nulla semper risus enim, ac vehicula elit rutrum
+        nec. Aliquam non aliquet velit. Ut interdum massa nec ultricies
+        vulputate. Cras quis elit ex. Suspendisse magna tellus, condimentum quis
+        tellus a, congue varius mi. Sed placerat commodo purus, a venenatis eros
+        pretium ac. Aenean efficitur ante mauris, eget sodales tortor aliquam
+        facilisis. Etiam dignissim vel massa non ultrices. Vivamus sit amet nisi
+        id libero vestibulum imperdiet. Nullam semper pellentesque risus eget
+        feugiat.
+      </p>
+      <Gallery photos={photos} />;
+    </div>
+  );
 };
 
 export default Project;
