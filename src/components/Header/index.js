58f07daaf16486f2ee138df59c18ca084f51f239
mstanka <mice78.mm@gmail.com>
2021-05-23 17:00:49 +0200

    separate Header in a component

---
+++ src/components/Header/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './style.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-wrapper">
+        <div className="logo">Logo</div>
+        <nav className="nav">
+          <ul>
+            <li>
+              <Link to="/" className="nav-link">
+                Domů
+              </Link>
+            </li>
+            <li>
+              <Link to="/o-projektu" className="nav-link">
+                O projektu
+              </Link>
+            </li>
+            <li>
+              <Link to="/o-nas" className="nav-link">
+                O nás
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="img-main"></div>
+    </header>
+  );
+};
+
+export default Header;
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Project from './pages/Project';
+import Header from './components/Header';
 import Footer from './components/Footer';
 import './style.css';
 
@@ -11,28 +12,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <div className="container">
-        <header className="header">
-          <nav className="nav">
-            <ul>
-              <li>
-                <Link to="/" className="nav-link">
-                  Domů
-                </Link>
-              </li>
-              <li>
-                <Link to="/o-projektu" className="nav-link">
-                  O projektu
-                </Link>
-              </li>
-              <li>
-                <Link to="/o-nas" className="nav-link">
-                  O nás
-                </Link>
-              </li>
-            </ul>
-          </nav>
-          <div className="img-main"></div>
-        </header>
+        <Header />
         <main>
           <Switch>
             <Route path="/o-projektu">
